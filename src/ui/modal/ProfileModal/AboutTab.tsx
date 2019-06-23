c7254279dabd0af79737eb4f9e06771b4a33cbd3
Shamil Khalilov <shamfama@gmail.com>
2019-06-23 19:18:36 +0300

    Modal tabs finish

--- src/ui/modal/ProfileModal/AboutTab.tsx
+++ src/ui/modal/ProfileModal/AboutTab.tsx
@@ -14,7 +14,28 @@ const AboutTab: React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
             <h3>About tab</h3>
             <span>Add More Information about yourself</span>
             <div className={bem.element('grid')}>
-                
+                <div>
+                    <span>Avatar URL</span>
+                    <InputField />
+                    <Button className="base-green">Connect</Button>
+                    <img src="#" alt="#"/>
+                </div>
+                <div>
+                    <span>Your Occupation</span>
+                    <InputField placeholder="Enter Your Occupation / Specialty"/>
+                </div>
+                <div>
+                    <span>Tags</span>
+                    <div>
+                        <label>Use ‘Enter’ to add a hashtag (10 max)</label>
+                        <InputField placeholder="Enter Tags"/>
+                        <div></div>
+                    </div>
+                </div>
+                <div>
+                    <span>Your Country</span>
+                    <InputField placeholder="Enter"/>
+                </div>
             </div>
         </div>
     );
